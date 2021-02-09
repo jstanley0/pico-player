4d04a6fe0adb8823766c6118275c180c4006614a
Jeremy Stanley <jeremy@instructure.com>
2021-02-09 08:05:07 -0700

    I'd say it compiles but it's interpreted
    e.g., it runs, and it produces output. I can't evaluate how
    good the output is until I write the thing that consumes it :P

--- util/convert_midi.py
+++ util/convert_midi.py
@@ -39,11 +39,12 @@ class Event:
 
 
 class Encoder:
-    def __init__(self, outfile, all_channels, channel_priority):
-        self.preferred_index = __compute_preferred_index(all_channels, channel_priority)
+    def __init__(self, outfile, channel_priority):
         self.notes_playing = [None, None, None, None, None, None]
         self.events = []
-        self.outfile = outfile
+        self.outfile = open(outfile, 'wb')
+        self.__compute_channel_sort_keys(channel_priority)
+        self.__compute_preferred_voice(channel_priority)
 
     def log_delay(self, delay):
         if self.events and not self.events[-1].notes_on() and not self.events[-1].notes_off():
@@ -53,6 +54,8 @@ class Encoder:
             self.events.append(Event(delay))
 
     def log_note_on(self, note, channel, velocity):
+        if channel == 10:
+            return  # TODO: map percussion to noise channels
         self.__ensure_event()
         self.events[-1].add_note_on((note, channel, velocity))
 
@@ -62,39 +65,52 @@ class Encoder:
 
     def finish(self):
         self.__write_last_event()
+        self.outfile.close()
 
     def __ensure_event(self):
         if not self.events:
             self.events.append(Event(0))
 
-    def __compute_preferred_index(all_channels, channel_priority):
-        if channel_priority:
-            pass
-        else:
-            pass
+    def __compute_preferred_voice(self, channel_priority):
+        order = [0, 5, 1, 4, 2, 3]
+        ix = 0
+        self.preferred_voice = {}
+        for ch in channel_priority:
+            self.preferred_voice[ch] = order[ix]
+            ix = (ix + 1) % len(order)
+        print('preferred_voice')
+        print(self.preferred_voice)
+
+    def __compute_channel_sort_keys(self, channel_priority):
+        key = 0
+        self.channel_sort_keys = {}
+        for ch in channel_priority:
+            self.channel_sort_keys[ch] = key
+            key += 1
+        print('channel_sort_keys')
+        print(self.channel_sort_keys)
 
     def __place_note(self, note):
         try:
-            i = self.preferred_index[note[1]]
-            if self.notes_playing[i] == None:
+            v = self.preferred_voice[note[1]]
+            if self.notes_playing[v] == None:
                 # easy: our preferred voice is available
-                return i
-            elif i < 3:
+                return v
+            elif v < 3:
                 # try to find a voice on the left channel
                 # but prefer spilling to the right to dropping entirely
-                for j in range(6):
-                    if self.notes_playing[j] == None:
-                        return j
+                for w in range(6):
+                    if self.notes_playing[w] == None:
+                        return w
             else:
                 # try to find a voice on the right channel
-                for j in range(5,-1,-1):
-                    if self.notes_playing[j] == None:
-                        return j
+                for w in range(5,-1,-1):
+                    if self.notes_playing[w] == None:
+                        return w
             return None
-
         except KeyError:
             # this channel is excluded
-            pass
+            return None
 
     def __write_last_event(self):
         if not self.events:
@@ -102,94 +118,104 @@ class Encoder:
         event = self.events[-1]
 
         # sanity check
-        print('writing event:')
-        print(delay)
-        print(event.notes_off)
-        print(event.notes_on)
-        print('---')
+        print('event:')
+        print(event.delay())
+        print(event.notes_off())
+        print(event.notes_on())
 
         # write delay
-        self.__write_delay(self.delay)
+        self.__write_delay(event.delay())
 
         # write notes off
         for note_off in event.notes_off():
             try:
-                i = self.notes_playing.index(note_off)
-                self.notes_playing[i] = None
-                self.__write_note_off(i)
+                v = self.notes_playing.index(note_off)
+                self.notes_playing[v] = None
+                self.__write_note_off(v)
             except ValueError:
                 # we probably had to drop this note earlier
                 # or maybe we're ignoring this channel entirely
                 pass
 
         # write notes on
-        # FIXME sort by channel priority somehow!
-        for note_on in event.notes_on():
-            i = self.__place_note(note_on)
-            if i != None
-                self.notes_playing[i] = (note_on[0], note_on[1])
-                self.__write_note_on(i, note_on[0], note_on[2]
+        filtered_notes_on = filter(lambda note: note[1] in self.channel_sort_keys, event.notes_on())
+        notes_on = sorted(filtered_notes_on, key=lambda note: self.channel_sort_keys[note[1]])
+        for note_on in notes_on:
+            v = self.__place_note(note_on)
+            if v != None:
+                self.notes_playing[v] = (note_on[0], note_on[1])
+                self.__write_note_on(v, note_on[0], note_on[2])
+
+        print('notes_playing:')
+        print(self.notes_playing)
+        print('---')
 
     def __midi_note_to_frequency(self, midi_note):
-        return int(round(CLOCK_FREQ / (32 * 440.0 * math.pow(2, (midi_note - 69.0) / 12))))
+        return round(CLOCK_FREQ / (32 * 440.0 * math.pow(2, (midi_note - 69.0) / 12)))
 
     def __midi_velocity_to_dynamic(self, velocity):
         return int(velocity / 32)
 
-    def __decode_index(self, index):
-        if index < 3:
-            return (0, index)
+    def __decode_voice(self, v):
+        if v < 3:
+            return (0, v)
         else:
-            return (1, index - 3)
+            return (1, v - 3)
 
     # delay: D = delay in milliseconds
     # 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
     #  1  1 DD DC DB DA D9 D8 D7 D6 D5 D4 D3 D2 D1 D0
     def __write_delay(self, delay):
+        delay = round(delay * 1000)
         while delay > 0x3FFF:
-            __write16(0xFFFF)
+            self.__write16(0xFFFF)
             delay -= 0x3FFF
         if delay > 0:
-            __write16(0xC000 | delay)
+            self.__write16(0xC000 | delay)
 
     # note on: C = channel; V = voice; D = dynamic; F = freq
     # 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
     #  0 C0 V1 V0 D1 D0 F9 F8 F7 F6 F5 F4 F3 F2 F1 F0
-    def __write_note_on(self, index, note, velocity):
-        channel, voice = self.__decode_index(index)
+    def __write_note_on(self, v, note, velocity):
+        channel, voice = self.__decode_voice(v)
         frequency = self.__midi_note_to_frequency(note)
         dynamic = self.__midi_velocity_to_dynamic(velocity)
         u16 = (channel & 1) << 14
         u16 |= (voice & 3) << 12
         u16 |= (dynamic & 3) << 10
         u16 |= (frequency & 0x3FF)
-        __write16(u16)
+        self.__write16(u16)
 
     # note off: C = channel; V = voice
     #  7  6  5  4  3  2  1  0
-    #  1  0  x  x  x C0 V1 V0
-    def __write_note_off(self, index):
-        channel, voice = self.__decode_index(index)
+    #  1  0  0  0  0 C0 V1 V0
+    def __write_note_off(self, v):
+        channel, voice = self.__decode_voice(v)
         u8 = 0x80
         u8 |= (channel & 1) << 2
         u8 |= (voice & 3)
-        __write8(u8)
+        self.__write8(u8)
 
     def __write16(self, u16):
-        self.outfile.write(u16.to_bytes(2, byteorder='big', signed=False)
+        self.outfile.write(u16.to_bytes(2, byteorder='big', signed=False))
 
     def __write8(self, u8):
-        self.outfile.write(u8.to_bytes(1, byteorder='big', signed=False)
+        self.outfile.write(u8.to_bytes(1, byteorder='big', signed=False))
 
 
 midi = MidiFile(args.infile)
 
-# I feel like there should be a better way to enumerate channels, but whatevs...
-all_channels = set()
-for msg in midi:
-    all_channels.add(msg.channel)
+# NOTE: 1 is added to channels to match user-visible channel numbers in e.g. MuseScore
 
-encoder = Encoder(args.outfile, all_channels, args.channel_priority)
+# I feel like there should be a better way to enumerate channels, but whatevs...
+channel_priority = args.channel_priority
+if not channel_priority:
+    channel_priority = set()
+    for msg in midi:
+        if msg.type == 'note_on':
+            channel_priority.add(msg.channel + 1)
+
+encoder = Encoder(args.outfile, channel_priority)
 for msg in midi:
     if msg.time > 0:
         encoder.log_delay(msg.time)

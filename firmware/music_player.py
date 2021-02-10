70a886970a75d01b3e488a086461b07d0d1d4858
Jeremy Stanley <jeremy@instructure.com>
2021-02-10 08:19:33 -0700

    envelope handling and cleanup
    protip: ALWAYS deinit your timer in a finally block

--- firmware/music_player.py
+++ firmware/music_player.py
@@ -1,5 +1,5 @@
 import utime
-from machine import Pin, PWM
+from machine import Pin, PWM, Timer
 from sound import Sound
 
 def read_words(filename):
@@ -15,40 +15,47 @@ def read_words(filename):
                 i += 2
 
 class MusicPlayer:
-    DYNAMICS = [(9, 14), (6, 12), (3, 10), (0, 8)]
+    DYNAMICS = [(9, 13), (6, 10), (3, 8), (0, 6)]
     LED_PINS = [16, 17, 18, None, 19, 20, 21, None]
     def __init__(self):
         self.sound = Sound()
         self.atten = [15] * 8
         self.target = [15] * 8
         self.__init_leds()
+        self.timer = Timer()
+        __g_player = self
 
     def play_song(self, filename):
-        for word in read_words(filename):
-            if word & 0x8000 == 0:
-                # note on: C = channel; V = voice; D = dynamic; F = freq
-                # 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
-                #  0 C0 V1 V0 D1 D0 F9 F8 F7 F6 F5 F4 F3 F2 F1 F0
-                freq = word & 0x3FF
-                dynamic = (word & 0xC00) >> 10
-                voice = (word & 0x7000) >> 12
-                print(freq)
-                self.__note_on(voice, freq, dynamic)
+        try:
+            self.timer.init(freq=20, mode=Timer.PERIODIC, callback=self.__process_envelopes)
+            for word in read_words(filename):
+                if word & 0x8000 == 0:
+                    # note on: C = channel; V = voice; D = dynamic; F = freq
+                    # 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+                    #  0 C0 V1 V0 D1 D0 F9 F8 F7 F6 F5 F4 F3 F2 F1 F0
+                    freq = word & 0x3FF
+                    dynamic = (word & 0xC00) >> 10
+                    voice = (word & 0x7000) >> 12
+                    self.__note_on(voice, freq, dynamic)
 
-            elif word & 0xC000 == 0x8000:
-                # delay: D = delay in milliseconds
-                # 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
-                #  1  0 DD DC DB DA D9 D8 D7 D6 D5 D4 D3 D2 D1 D0
-                ms = word & 0x3FFF
-                # TODO use utime.ticks_ms() et al to deduct busy time from sleep time and avoid song slowdowns
-                utime.sleep_ms(ms)
+                elif word & 0xC000 == 0x8000:
+                    # delay: D = delay in milliseconds
+                    # 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+                    #  1  0 DD DC DB DA D9 D8 D7 D6 D5 D4 D3 D2 D1 D0
+                    ms = word & 0x3FFF
+                    # TODO use utime.ticks_ms() et al to deduct busy time from sleep time and avoid song slowdowns
+                    utime.sleep_ms(ms)
 
-            elif word & 0xC000 == 0xC000:
-                # notes off: C = channel; L = left chip; R = right chip
-                # 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
-                #  1  1  0  0  0  0  0  0 R3 R2 R1 R0 L3 L2 L1 L0
-                mask = word & 0xFF
-                self.__notes_off(mask)
+                elif word & 0xC000 == 0xC000:
+                    # notes off: C = channel; L = left chip; R = right chip
+                    # 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+                    #  1  1  0  0  0  0  0  0 R3 R2 R1 R0 L3 L2 L1 L0
+                    mask = word & 0xFF
+                    self.__notes_off(mask)
+        finally:
+            self.__lights_off()
+            self.sound.silence()
+            self.timer.deinit()
 
     def __init_leds(self):
         self.pwms = []
@@ -60,6 +67,11 @@ class MusicPlayer:
                 pwm.freq(120)
                 pwm.duty_u16(0)
                 self.pwms.append(pwm)
+
+    def __lights_off(self):
+        for pwm in self.pwms:
+            if pwm:
+                pwm.duty_u16(0)
 
     def __set_led_intensity(self, voice, atten):
         if self.pwms[voice]:
@@ -78,14 +90,11 @@ class MusicPlayer:
         for voice in range(8):
             if 0 != (mask & (1 << voice)):
                 self.target[voice] = 15
-                # TODO remove the following lines after envelope is implemented
-                self.atten[voice] = 15
-                self.sound.set_attenuation(voice, 15)
-                self.__set_led_intensity(voice, 15)
 
-    def __process_envelopes(self):
+    def __process_envelopes(self, _timer):
         for voice in range(8):
             if self.atten[voice] < self.target[voice]:
                 self.atten[voice] += 1
                 self.sound.set_attenuation(voice, self.atten[voice])
                 self.__set_led_intensity(voice, self.atten[voice])
+
